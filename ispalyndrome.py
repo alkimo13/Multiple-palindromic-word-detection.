74084618e1e57165b36f671e69d4a95b392f4e49
Alkım Bozkurt <78503740+alkimo13@users.noreply.github.com>
2021-02-03 22:32:08 +0300

    Update ispalyndrome.py

--- ispalyndrome.py
+++ ispalyndrome.py
@@ -3,5 +3,18 @@ def ispaly(st1):
         return True
     else:
         return False
-
-
+a =  "Madam Tssaud has just came from kayak. Her daughter Anna was waiting her mom while the phone rang"
+aa = []
+b = 0
+x =0
+y = 1
+a = a.lower()
+while y<len(a)+1:
+    aa.append(a[b:y])
+    y = y+1
+    if y == len(a)+1:
+        b=b+1
+        y =b+1
+for i in list(set(aa)):
+    if ispaly(i) and len(i)>1:
+        print(i.strip())
